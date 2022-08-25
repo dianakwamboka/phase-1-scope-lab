32d62d3dd31a639aed8e4cf98c45407b3193d0ac
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-25 12:05:45 +0300

    Comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+//
+console.log(customerName);
+//
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+//
+var bestCustomer;
+//
+function setBestCustomer() { 
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+//
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+//
+const leastFavoriteCustomer = 'Mwongola';
+//
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'adix';
+    return leastFavoriteCustomer;
+}
